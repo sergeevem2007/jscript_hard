0120588210aebb4d41d3434b30703f287f8621b7
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-03-31 00:09:46 +0400

    feat:add function langIF,langSwitch,langArray. add

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+'use strict';
+
 let num = 266219;
+let lang;
+let ru = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+let en = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+let lang_array = [];
+let namePerson;  
 
 function opNumbers(num) {
   if (!num)
@@ -14,3 +21,39 @@ function opNumbers(num) {
   return result; 
 }
 opNumbers(num);
+
+lang = prompt('введите значение "ru" или "en"');
+function langIf(lang) {
+  if (lang === 'ru') {
+    console.log(ru);
+  } else {
+    if (lang === 'en') {
+      console.log(en);
+    } else console.log('Введены некоретные значения');
+  }
+}
+function langSwitch(lang) {
+  switch(lang){
+    case 'ru':
+      console.log(ru);
+      break;
+    case 'en':
+      console.log(en);
+      break;
+  }
+}
+function langArray(lang) { 
+  lang_array['ru'] = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+  lang_array['en'] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+  console.log(lang_array[lang]);
+}
+
+langIf(lang);
+langSwitch(lang);
+langArray(lang);
+
+namePerson = prompt('введите значение namePerson');
+namePerson.toLowerCase();
+(namePerson === 'артем') ? console.log('директор') :
+(namePerson === 'максим') ? console.log('преподаватель') :
+console.log('студент');
