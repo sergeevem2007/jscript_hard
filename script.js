a5c410e37f3df49842d1c23f5126737e1755cfe6
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-04-01 01:32:10 +0400

    refact:change function opNumbers

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let num = 266219;
 
-function opNumbers(num) {
-  if (!num)
-      return 0;
-  let result = 1;
-  while (num) {
-      result *= num % 10;
-      num = Math.floor(num / 10);
-  }
-  result = result ** 3;
-  result = String(result);
-  console.log(result[0] + ' ' + result[1]);
+function opNumbers(num) { 
+  if (!num) 
+    return 0; 
+  let result = 1; 
+  while (num) { 
+    result *= num % 10; 
+    num = Math.floor(num / 10); 
+  } 
+  result = result ** 3; 
+  console.log(result); 
+  let arr = []; 
+  while (result) { 
+    arr.unshift(result % 10); 
+    result = Math.floor(result / 10); 
+  } 
+  console.log(arr[0] , arr[1]); 
   return result; 
-}
-opNumbers(num);
+  }
+
+  opNumbers(num);
