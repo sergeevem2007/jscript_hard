8428f832dc80e3b3a5d908be9ec8088ba0927fd0
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-04-15 00:20:22 +0400

    refact:chnage strings cssText

--- script.js
+++ script.js
@@ -14,20 +14,26 @@ DomElement.prototype.createElem = function(){
   if (this.selector.charAt(0) === '.') {
     let div = document.createElement('div');
     div.classList.add(this.selector.slice(1));
-    div.style.cssText=`height: `+ this.height +`px;
-    width: `+ this.width +`px;
-    background:`+ this.bg +`;
-    font-size:`+ this.fontSize +`px;`;
+    div.style.cssText=
+    `
+    height: ${this.height}px;
+    width: ${this.width}px;
+    background:${this.bg};
+    font-size:${this.fontSize}px;
+    `;
     div.innerHTML = 'Всю отвергает если, потому, не истину приносило того по: вы я умеет было раз. Наслаждение умеет порицающих предаваться, за страдания из-за презирает справедливости это я. Стал неприятностей из никого, такого то воспользоваться, вами всю разумно вами никто, открывший перед постигают приносят возлюбил:'+
     'Никто нас возлюбил воспользоваться физическими отвергает из никаких, наслаждений предпочел упрекнуть истину никаких разъясню из. Постигают само всю, назвал — избегает не нет упражнениями раскрою пользы поняли.';
     body.append(div);
   } else if (this.selector.charAt(0) === '#') {
     let p = document.createElement('p');
     p.id = this.selector.slice(1);
-    p.style.cssText=`height: `+ this.height +`px;
-    width: `+ this.width +`px;
-    background:`+ this.bg +`;
-    font-size:`+ this.fontSize +`px;`;
+    p.style.cssText=
+    `
+    height: ${this.height}px;
+    width: ${this.width}px;
+    background:${this.bg};
+    font-size:${this.fontSize}px;
+    `;
     p.innerHTML = 'Всю отвергает если, потому, не истину приносило того по: вы я умеет было раз. Наслаждение умеет порицающих предаваться, за страдания из-за презирает справедливости это я. Стал неприятностей из никого, такого то воспользоваться, вами всю разумно вами никто, открывший перед постигают приносят возлюбил:';
     body.append(p);
   }
