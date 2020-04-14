d2c973ad57d8d1955c5047f5598b86bf35b5c15e
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-04-14 22:17:39 +0400

    feat:add DomElement

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 'use strict';
 
-let color = document.querySelector('#color'),
-    container = document.querySelector('.container'),
-    button = document.querySelector('button'),
-    arr = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'],
-    colorValue;
+function DomElement(selector, height, width, bg, fontSize){
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+}
 
-let getRandomInt = function() {
-  let min = 0;
-  let max = 16;
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
-};
-
-let changeColor =  function() {
-  let colorValue = '';
-  for (let i = 0; i < 6; i++) {
-    let random = arr[getRandomInt()].toString();
-    colorValue += random;
+DomElement.prototype.createElem = function(){
+  let body = document.querySelector('body');
+  console.log(this);
+  if (this.selector.charAt(0) === '.') {
+    let div = document.createElement('div');
+    div.classList.add(this.selector.slice(1));
+    div.style.cssText=`height: `+ this.height +`px;
+    width: `+ this.width +`px;
+    background:`+ this.bg +`;
+    font-size:`+ this.fontSize +`px;`;
+    div.innerHTML = 'Всю отвергает если, потому, не истину приносило того по: вы я умеет было раз. Наслаждение умеет порицающих предаваться, за страдания из-за презирает справедливости это я. Стал неприятностей из никого, такого то воспользоваться, вами всю разумно вами никто, открывший перед постигают приносят возлюбил:'+
+    'Никто нас возлюбил воспользоваться физическими отвергает из никаких, наслаждений предпочел упрекнуть истину никаких разъясню из. Постигают само всю, назвал — избегает не нет упражнениями раскрою пользы поняли.';
+    body.append(div);
+  } else if (this.selector.charAt(0) === '#') {
+    let p = document.createElement('p');
+    p.id = this.selector.slice(1);
+    p.style.cssText=`height: `+ this.height +`px;
+    width: `+ this.width +`px;
+    background:`+ this.bg +`;
+    font-size:`+ this.fontSize +`px;`;
+    p.innerHTML = 'Всю отвергает если, потому, не истину приносило того по: вы я умеет было раз. Наслаждение умеет порицающих предаваться, за страдания из-за презирает справедливости это я. Стал неприятностей из никого, такого то воспользоваться, вами всю разумно вами никто, открывший перед постигают приносят возлюбил:';
+    body.append(p);
   }
-  color.innerHTML = '#' + colorValue;
-  button.style.color = '#' + colorValue;
-  container.style.backgroundColor = '#' + colorValue;
 };
 
+let elem1 = new DomElement ('.block','400','500','red','20');
+let elem2 = new DomElement ('#best','500','900','green','30');
 
-button.addEventListener('click', changeColor);
+elem1.createElem();
+elem2.createElem();
+
