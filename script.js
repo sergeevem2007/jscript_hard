e18d69e62829c96a9c1c8f1ba4e5a905041fbf77
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-04-16 19:36:00 +0400

    refact:change getRandomInt

--- script.js
+++ script.js
@@ -2,25 +2,18 @@
 
 let color = document.querySelector('#color'),
     container = document.querySelector('.container'),
-    button = document.querySelector('button'),
-    arr = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'],
-    colorValue;
+    button = document.querySelector('button');
 
 let getRandomInt = function() {
-  let min = 0;
-  let max = 16;
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  return '#' + Math.floor(Math.random()*16777215).toString(16); // 16777215 - это колличество возможных цветов в палитре
 };
 
 let changeColor =  function() {
-  let colorValue = '';
-  for (let i = 0; i < 6; i++) {
-    let random = arr[getRandomInt()].toString();
-    colorValue += random;
-  }
-  color.innerHTML = '#' + colorValue;
-  button.style.color = '#' + colorValue;
-  container.style.backgroundColor = '#' + colorValue;
+  let colorValue = getRandomInt();
+  
+  color.innerHTML = colorValue;
+  button.style.color = colorValue;
+  container.style.backgroundColor = colorValue;
 };
 
 
