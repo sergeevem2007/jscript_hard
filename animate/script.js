8e099ce023b2397e366e6bfdeb255b4f31c3ac3e
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-05-05 13:30:11 +0400

    feat:add start, pause, reset property

--- animate/script.js
+++ animate/script.js
@@ -1,27 +1,39 @@
 'use strict';
 
 
-let elem = document.getElementById('my-element'),
-    start = document.getElementById('start');
+let element = document.getElementById('element'),
+    start = document.getElementById('start'),
+    pause = document.getElementById('pause'),
+    reset = document.getElementById('reset'),
+    moveInterval,
+    count;
 
-const startAnimate = () => {
-  let count = 0,
-  moveInterval,
-  moveAnimate = function(){
-  moveInterval = requestAnimationFrame(moveAnimate);
-  count += 0.3;
-  if (count <= 75) {
-    elem.style.left = count + '%';
-  } else {
-    count = 0;
-  }
-  }
-  moveInterval = requestAnimationFrame(moveAnimate);  
-};
-
+const moveAnimate = function () {
+    moveInterval = requestAnimationFrame(moveAnimate);
+    count += 0.3;
+    if (count <= 75) {
+        element.style.left = count + '%';
+        element.style.transform = `rotate(${count * 10}deg)`;
+    } else {
+        count = 0;
+    }
+    return count;
+}
 start.addEventListener('click', () => {
-  startAnimate();
-})
-
-
-
+  moveAnimate();
+  start.classList.toggle('d-none');
+  pause.classList.toggle('d-none');
+});
+pause.addEventListener('click', () => {
+  moveInterval = cancelAnimationFrame(moveInterval);
+  start.classList.toggle('d-none');
+  pause.classList.toggle('d-none');
+});
+reset.addEventListener('click', () => {
+  moveInterval = cancelAnimationFrame(moveInterval);
+  count = 0;
+  start.classList.remove('d-none');
+  pause.classList.add('d-none');
+  element.style.left = `${count}`;
+  element.style.transform = `rotate(${count}deg)`;
+});
